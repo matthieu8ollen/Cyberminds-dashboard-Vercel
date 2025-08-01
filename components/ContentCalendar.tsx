7a8996763dfd80fbbd0320a47a8b4a86a2991d98
matthieu8ollen <matthieu@cybermindsagency.com>
2025-08-01 16:41:30 +0200

    Update ContentCalendar.tsx

--- components/ContentCalendar.tsx
+++ components/ContentCalendar.tsx
@@ -714,7 +714,7 @@ export default function ContentCalendar() {
                     onChange={(e) => setRescheduleDate(e.target.value)}
                     className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-teal-500 focus:border-transparent pr-10"
                   />
-                  <Calendar className="absolute right-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400 pointer-events-none" />
+                  <CalendarIcon className="absolute right-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400 pointer-events-none" /><CalendarIcon className="absolute right-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400 pointer-events-none" />
                 </div>
               </div>
               
