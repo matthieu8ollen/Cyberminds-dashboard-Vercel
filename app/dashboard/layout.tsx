aa56f28a2726678919544be96316926fe8c56477
matthieu8ollen <matthieu@cybermindsagency.com>
2025-08-11 12:00:41 +0200

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import { AuthProvider } from '../../contexts/AuthContext'
-import { ContentProvider } from '../../contexts/ContentContext'
+import { ContentProvider } from '../../contexts/ContentProvider'
 import { WorkflowProvider } from '../../contexts/WorkflowContext'
 import { ToastProvider } from '../../components/ToastNotifications'
-
 import BreadcrumbNavigation from '../../components/BreadcrumbNavigation'
+import Dashboard from '../../components/Dashboard'
 
 export default function DashboardLayout({
   children
@@ -17,9 +17,17 @@ export default function DashboardLayout({
       <ContentProvider>
         <WorkflowProvider>
           <ToastProvider>
-            <div className="min-h-screen bg-gray-50">
-              <BreadcrumbNavigation />
-              {children}
+            <div className="flex min-h-screen bg-gray-50">
+              {/* Dashboard Sidebar */}
+              <Dashboard />
+              
+              {/* Main Content Area */}
+              <div className="flex-1 flex flex-col">
+                <BreadcrumbNavigation />
+                <main className="flex-1">
+                  {children}
+                </main>
+              </div>
             </div>
           </ToastProvider>
         </WorkflowProvider>
