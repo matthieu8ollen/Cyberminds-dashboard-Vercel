c64fe12ff655f1299838f1a26d38b159da20fee8
matthieu8ollen <matthieu@cybermindsagency.com>
2025-08-11 12:36:37 +0200

    Update WriterSuiteSelection.tsx

--- components/WriterSuiteSelection.tsx
+++ components/WriterSuiteSelection.tsx
@@ -48,7 +48,7 @@ interface WriterSuiteSelectionProps {
   onModeSelect: (mode: 'writer-suite' | 'standard') => void
 }
 
-export default function WriterSuiteSelection({ onModeSelect, onBack }: WriterSuiteSelectionProps) {
+export default function WriterSuiteSelection({ onModeSelect }: WriterSuiteSelectionProps) {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       {/* Header */}
