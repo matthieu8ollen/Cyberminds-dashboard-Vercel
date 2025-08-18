b786e6af9a801a7b1471cdadb201ac25983c5627
matthieu8ollen <matthieu@cybermindsagency.com>
2025-08-18 13:58:15 +0200

    Update WriterSuite.tsx

--- components/WriterSuite.tsx
+++ components/WriterSuite.tsx
@@ -5,13 +5,15 @@ import MarcusCopilot from './MarcusCopilot'
 interface WriterSuiteProps {
   onComplete?: (data: any) => void
   onBack?: () => void
+  ideationData?: any
 }
 
-export default function WriterSuite({ onComplete, onBack }: WriterSuiteProps) {
+export default function WriterSuite({ onComplete, onBack, ideationData }: WriterSuiteProps) {
   return (
     <MarcusCopilot 
       onComplete={onComplete}
       onBack={onBack}
+      ideationData={ideationData}
     />
   )
 }
