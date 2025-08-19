7dfb54e1f21418b4fe169abc95aa8c9fe8d3ec17
matthieu8ollen <matthieu@cybermindsagency.com>
2025-08-19 12:05:43 +0200

    Update WriterSuite.tsx

--- components/WriterSuite.tsx
+++ components/WriterSuite.tsx
@@ -6,14 +6,24 @@ interface WriterSuiteProps {
   onComplete?: (data: any) => void
   onBack?: () => void
   ideationData?: any
+  onExitWorkflow?: () => void
+  onContinueToImages?: (contentId: string) => void
 }
 
-export default function WriterSuite({ onComplete, onBack, ideationData }: WriterSuiteProps) {
+export default function WriterSuite({ 
+  onComplete, 
+  onBack, 
+  ideationData, 
+  onExitWorkflow, 
+  onContinueToImages 
+}: WriterSuiteProps) {
   return (
-    <MarcusCopilot 
-      onComplete={onComplete}
-      onBack={onBack}
-      ideationData={ideationData}
-    />
-  )
+  <MarcusCopilot 
+    onComplete={onComplete}
+    onBack={onBack}
+    ideationData={ideationData}
+    onExitWorkflow={onExitWorkflow}
+    onContinueToImages={onContinueToImages}
+  />
+)
 }
