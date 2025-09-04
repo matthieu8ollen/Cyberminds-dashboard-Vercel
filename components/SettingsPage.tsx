2f6d0a982301a8173d85c2a466f3052b6f1f660d
matthieu8ollen <matthieu@cybermindsagency.com>
2025-09-04 14:38:43 +0200

    Update SettingsPage.tsx

--- components/SettingsPage.tsx
+++ components/SettingsPage.tsx
@@ -1,6 +1,6 @@
-'use client'
+'use client';
 
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { useAuth } from '../contexts/AuthContext'
 import { updateUserProfile } from '../lib/supabase'
 import { useLinkedInAuth } from '../lib/linkedInAPI'
